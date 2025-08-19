2b12a456767d093883da5978c49eefb145ea10fe
capituralike <messiasfranca1971@gmail.com>
2025-08-18 21:13:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,6 +97,5 @@ def mudar(lead_id, etapa):
     conn.commit()
     conn.close()
     return redirect(url_for("admin"))
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
